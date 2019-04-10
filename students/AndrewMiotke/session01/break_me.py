55b8a8fbed3f1d025a2ebb71bd00ef977772eb61
Andrew Miotke <andrewmiotke@gmail.com>
2019-04-09 21:01:03 -0700

    initial break_me.py

---
+++ students/AndrewMiotke/session01/break_me.py
@@ -0,0 +1,16 @@
+def NameError():
+    printx("Here's a name error")
+    
+NameError()
+
+def TypeError():
+
+TypeError()
+
+def SyntaxError():
+
+SyntaxError()
+
+def AttributeError():
+
+AttributeError()

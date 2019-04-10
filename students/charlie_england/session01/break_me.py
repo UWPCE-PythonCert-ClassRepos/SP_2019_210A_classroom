96a0345fe949dc5eccb47a08f06ad462d27bff93
Charlie England <charlie265@live.com>
2019-04-09 21:02:22 -0700

    added NameError

---
+++ students/charlie_england/session01/break_me.py
@@ -0,0 +1,20 @@
+'''
+breakme le gasp
+NameError, TypeError, SyntaxError, AttributeError
+Create 4 functions that will give each error, syntax error will happen before code is run
+'''
+
+def name_error():
+    #referencing something that does not exist
+    non_existant_variable + 1
+
+def type_error():
+    pass
+
+def syntax_error():
+    pass
+
+def attribute_error():
+    pass
+
+name_error()

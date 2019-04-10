f6c63cc6ffd134bbfab9642dc73edd3f436ad99b
Lincoln <kanmenlaohan@gmail.com>
2019-04-09 23:02:11 -0500

    add testing file

---
+++ students/Lincoln_Zhang/Session01/break_me.py
@@ -0,0 +1,10 @@
+#
+#Assignment1: break_me.py
+#generate 4 errors: name error; type error; syntax error; attribute error
+def name_error():
+    print ("the name error function")
+    printx("actual name error")
+
+name_error()
+
+

f87c2665b4293ca0c564134599d8438bf7c2fca4
JoeNunnelley <joe.nunnelley@socrata.com>
2019-04-09 21:01:09 -0700

    initial work for breakme file

---
+++ students/JoeNunnelley/session01/break_me.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+
+""" break me : output error handling """
+"""
+Errors:
+  NameError, TypeError, SyntaxError, AttributeError
+"""
+
+def name_error():
+    print("the name error function")
+    printx("hello")
+
+name_error()
+
+
+def type_error():
+    print("the type error function")
+
+type_error
+
+def attribute_error():
+    print("the attribute error function")
+
+attribute_error
+
+
+def syntax_error():
+    print("the syntax error function")
+
+syntax_error

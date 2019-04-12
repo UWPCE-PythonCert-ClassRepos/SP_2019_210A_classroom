48309721df4258d0c09261fb0378b1b4229387a6
Arch Wu <archwu0817@gmail.com>
2019-04-11 21:47:40 -0700

    finished assignment1

--- students/ArchWu/Session1/break_me.py
+++ students/ArchWu/Session1/break_me.py
@@ -3,4 +3,19 @@ def name_error():
     pintx("lel")
 name_error()
 
-just a change to test
+def type_error():
+    x = '1' + 1
+    print(x)
+
+type_error()
+
+def syntax_error():
+    print()
+
+syntax_error()
+
+def attribute_error():
+    x = 1
+    x.append(0)
+
+attribute_error()

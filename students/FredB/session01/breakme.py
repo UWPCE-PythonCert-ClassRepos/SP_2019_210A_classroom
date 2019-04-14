c3c906297f45a949c4e2b5d4582073f7e7f95d9c
Fred Ballyns <FredBallyns@gmail.com>
2019-04-14 10:48:31 -0700

    added attribute error

--- students/FredB/session01/breakme.py
+++ students/FredB/session01/breakme.py
@@ -4,21 +4,23 @@
 
 #NameError
 
-print(undefinedText)
+#print(undefinedText)
 
 
 #TypeError
 
 a=5
 b="5"
-c=a+b
+#c=a+b
 
 
 #SyntaxError
-"
+#   "
 
 
 
 #AttributeError
+List = 5
+List.append(4)
 
 

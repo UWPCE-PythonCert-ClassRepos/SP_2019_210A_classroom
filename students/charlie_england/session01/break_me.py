3b0fb2ba21cba25bd35ea70d40462591272aba2e
Charlie England <charlie265@live.com>
2019-04-14 21:53:22 -0700

    error testing

--- students/charlie_england/session01/break_me.py
+++ students/charlie_england/session01/break_me.py
@@ -9,10 +9,17 @@ def name_error():
     non_existant_variable + 1
 
 def type_error():
-    pass
+    #error when values cannot be combined together
+    "2" + 2
 
 def syntax_error():
+    #parsing errors
+    #fr x in range(1,10):
+        #print(x)
     pass
 
 def attribute_error():
-    pass
+    #trying to use an attribute (function) of an object when the function does not exist
+    integer = 8
+    integer.append(1) #cannot append to an integeter, can append to a list
+

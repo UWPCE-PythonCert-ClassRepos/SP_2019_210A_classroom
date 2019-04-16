b0eb1a84846ad4672c02ccbc87c6a9d7ece301df
Andrew Miotke <andrewmiotke@gmail.com>
2019-04-16 15:24:31 -0700

    Working grid generator

--- students/AndrewMiotke/session02/grid_printer.py
+++ students/AndrewMiotke/session02/grid_printer.py
@@ -21,77 +21,48 @@
 # verbose_grid()
 
 # More general grid generator
-def do_twice(f):
-    f()
-    f()
+def create_beam():
+    beam = '+ - - - - '
+    plus = "+"
+    end_line = end=' '
+    print(beam * 2 + plus + end_line)
 
-def do_four(f):
-    do_twice(f)
-    do_twice(f)
+def create_post():
+    post = '|         '
+    end_line = end=' '
+    print(post * 3)
 
-def do_six(f):
-    do_twice(f)
-    do_twice(f)
-    do_twice(f)
+def print_grid():
+    create_beam()
+    create_post()
+    create_post()
+    create_post()
 
-def print_beam():
-    print("+ - - - -", end=" ")
+    create_beam()
+    create_post()
+    create_post()
+    create_post()
 
-def print_post():
-    print("|        ", end=" ")
+    create_beam()
+    create_post()
+    create_post()
+    create_post()
 
-def print_beams():
-    do_twice(print_beam)
-    print("+")
+    create_beam()
+    create_post()
+    create_post()
+    create_post()
 
-def print_posts():
-    do_twice(print_post)
-    print("|")
+    create_beam()
+    create_post()
+    create_post()
+    create_post()
 
-def print_row():
-    print_beams()
-    do_four(print_posts)
+    create_beam()
+    create_post()
+    create_post()
+    create_post()
 
+    create_beam()
 
-def print_grid(t):
-    # do_twice(print_row)
-    print_row()
-    print_beams()
-
-print_grid(5)
-
-
-
-
-
-# def do_twice(f):
-#     f()
-#     f()
-
-# def do_four(f):
-#     do_twice(f)
-#     do_twice(f)
-
-# def print_beam():
-#     print('+ - - - -', end=' ')
-
-# def print_post():
-#     print('|        ', end=' ')
-
-# def print_beams():
-#     do_twice(print_beam)
-#     print('+')
-
-# def print_posts():
-#     do_twice(print_post)
-#     print('|')
-
-# def print_row():
-#     print_beams()
-#     do_four(print_posts)
-
-# def print_grid():
-#     do_twice(print_row)
-#     print_beams()
-
-# print_grid()
+print_grid()

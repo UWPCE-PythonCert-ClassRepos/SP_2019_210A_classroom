cb1f74ab01bf543008d08f494714afc94bdaa50b
BBrumleve <brian.brum#gmail.com>
2019-04-15 20:15:06 -0700

    correcting commenting out status of functions in break_me file

--- lesson01/break_me.py
+++ lesson01/break_me.py
@@ -6,26 +6,26 @@ def name_error():
 print(name_error())
 
 
-# TypeError
-def type_error(day):
-    date = ["month", "year"]
-    return(date)
-print(type_error())
-
-
-# I recall it was stated in class SyntaxError would not
-# be used.  Well here's my hack at it anyway.
-
-# SyntaxError
-def syntax_error(x,y):
-    sum = x + 3y
-    return(sum)
-print(syntax_error(2,3))
-
-
-# AttributeError
-def attribute_error():
-    age = 8
-    age = age.append(3) # integers cannot be appended. Python appends objects.
-    return(age)
-print(attribute_error())
+## TypeError
+#def type_error(day):
+#    date = ["month", "year"]
+#    return(date)
+#print(type_error())
+#
+#
+## I recall it was stated in class SyntaxError would not
+## be used.  Well here's my hack at it anyway.
+#
+## SyntaxError
+#def syntax_error(x,y):
+#    sum = x + 3y
+#    return(sum)
+#print(syntax_error(2,3))
+#
+#
+## AttributeError
+#def attribute_error():
+#    age = 8
+#    age = age.append(3) # integers cannot be appended. Python appends objects.
+#    return(age)
+#print(attribute_error())

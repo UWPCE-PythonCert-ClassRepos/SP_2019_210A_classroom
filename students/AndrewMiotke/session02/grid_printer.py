c84cbb09a02010f11f7659cb73346987c05c1aae
Andrew Miotke <andrewmiotke@gmail.com>
2019-04-16 16:14:22 -0700

    small 1:1 grid created

--- students/AndrewMiotke/session02/grid_printer.py
+++ students/AndrewMiotke/session02/grid_printer.py
@@ -2,21 +2,21 @@
 
 # Long hand way of creating a grid
 # def verbose_grid():
-#     print("+",  "-" * 4, "+", "-" * 4, "+")
+#     print("+",  " - " * 4, "+", " - " * 4, "+")
 
-#     print("|      |      |")
-#     print("|      |      |")
-#     print("|      |      |")
-#     print("|      |      |")
+#     print("|              |              |")
+#     print("|              |              |")
+#     print("|              |              |")
+#     print("|              |              |")
 
-#     print("+",  "-" * 4, "+", "-" * 4, "+")
+#     print("+",  " - " * 4, "+", " - " * 4, "+")
 
-#     print("|      |      |")
-#     print("|      |      |")
-#     print("|      |      |")
-#     print("|      |      |")
+#     print("|              |              |")
+#     print("|              |              |")
+#     print("|              |              |")
+#     print("|              |              |")
 
-#     print("+",  "-" * 4, "+", "-" * 4, "+")
+#     print("+",  " - " * 4, "+", " - " * 4, "+")
 
 # verbose_grid()
 
@@ -25,44 +25,23 @@ def create_beam():
     beam = '+ - - - - '
     plus = "+"
     end_line = end=' '
+
     print(beam * 2 + plus + end_line)
 
 def create_post():
     post = '|         '
-    end_line = end=' '
+
     print(post * 3)
 
+def create_grid():
+    create_beam()
+    create_post()
+    create_post()
+    create_post()
+
+    create_beam()
+
 def print_grid():
-    create_beam()
-    create_post()
-    create_post()
-    create_post()
-
-    create_beam()
-    create_post()
-    create_post()
-    create_post()
-
-    create_beam()
-    create_post()
-    create_post()
-    create_post()
-
-    create_beam()
-    create_post()
-    create_post()
-    create_post()
-
-    create_beam()
-    create_post()
-    create_post()
-    create_post()
-
-    create_beam()
-    create_post()
-    create_post()
-    create_post()
-
-    create_beam()
+    create_grid()
 
 print_grid()

9b32a682fd89483271733ff6a92aee7acffc6ad6
Andrew Miotke <andrewmiotke@gmail.com>
2019-04-16 09:22:39 -0700

    getting started on the grid printer

---
+++ students/AndrewMiotke/session02/grid_printer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def print_grid():
+    print("+",  "-" * 4, "+", "-" * 4, "+")
+    print("|\n" * 4, end = '') 
+    print("+",  "-" * 4, "+", "-" * 4, "+")
+    print("|\n" * 4, end = '')
+    print("+",  "-" * 4, "+", "-" * 4, "+")
+
+
+
+
+print_grid()
+

e69f090d53b0628756c8eb7d7da741e435eb9810
Charlie England <charlie265@live.com>
2019-04-16 20:00:25 -0700

    able to add grid size

--- students/charlie_england/session02/grid_printer.py
+++ students/charlie_england/session02/grid_printer.py
@@ -1,12 +1,25 @@
-def pri_grid():
-    for x in range(5):
+def pri_grid(num_rows=2, num_columns=2):
+    for x in range(num_rows*2+1):
         if x % 2 == 0:
-            print("+", "-"*4, "+", "-"*4, "+")  
+            for nc in range(num_columns*2+1):
+                if nc % 2 == 0:
+                    print("+ ", end="")
+                else:
+                    print("- "*4, end="")
+                if nc == num_columns*2:
+                    print("")
         else:
             for _ in range(3):
-                print("|", " "*4, "|", " "*4, "|")
+                for nc in range(num_columns*2+1):
+                    if nc % 2 == 0:
+                        print("|", end="")
+                    else:
+                        print(" "*9, end="")
+                    if nc == num_columns*2:
+                        print("")
 
-pri_grid()
+
+pri_grid(3, 3)
 # print("+","-"*4,"+","-"*4,"+")
 # for num in range(4):
 #    print("|", " "*4,"|", " "*4, "|")

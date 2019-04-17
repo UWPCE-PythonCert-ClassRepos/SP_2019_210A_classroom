1fb63a7b0f1aace4cdd1521351aea7b59be80b3c
Charlie England <charlie265@live.com>
2019-04-16 19:38:59 -0700

    optimized

--- students/charlie_england/session02/grid_printer.py
+++ students/charlie_england/session02/grid_printer.py
@@ -1,21 +1,16 @@
-def plus():
-    print("+","-"*4,"+","-"*4,"+")
+def pri_grid():
+    for x in range(5):
+        if x % 2 == 0:
+            print("+", "-"*4, "+", "-"*4, "+")  
+        else:
+            for _ in range(3):
+                print("|", " "*4, "|", " "*4, "|")
 
-def bar():
-    for num in range(4):
-        print("|", " "*4,"|", " "*4, "|")
-
-
-#print("+","-"*4,"+","-"*4,"+")
-#for num in range(4):
+pri_grid()
+# print("+","-"*4,"+","-"*4,"+")
+# for num in range(4):
 #    print("|", " "*4,"|", " "*4, "|")
-#print("+","-"*4,"+","-"*4,"+")
-#for num in range(4):
+# print("+","-"*4,"+","-"*4,"+")
+# for num in range(4):
 #    print("|", " "*4,"|", " "*4, "|")
-#print("+","-"*4,"+","-"*4,"+")
-
-for x in range(6):
-    if x %2 != 0:
-        plus()
-    else:
-        bar()
+# print("+","-"*4,"+","-"*4,"+")

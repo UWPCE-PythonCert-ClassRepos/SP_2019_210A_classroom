12a97abcc2555d4558a034a997f581264474faba
Andrew Miotke <andrewmiotke@gmail.com>
2019-04-16 22:18:35 -0700

    fib and lucas series are complete

--- students/AndrewMiotke/session02/series.py
+++ students/AndrewMiotke/session02/series.py
@@ -1,13 +1,23 @@
-def fibonacci(n):
+def fib(n):
     """Computes through the a series of numbers by summing the previous two numbers, see Fibonacci Number on wikipedia"""
-    for i in range(0, n):    
-        print((i-2) + (i-1))
-    return n
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-2) + fib(n-1)
 
-fibonacci(10)
+# Only printing to see the output. The tests should prove it's accuracy
+print(fib(10))
+
 
 def lucas(n):
     """The Lucas Numbers are a related series of integers that start with the values 2 and 1 rather than 0 and 1, see Lucas Number on wikipedia"""
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return lucas(n-2) + lucas(n-1)
 
-    return n
-
+print(lucas(10))

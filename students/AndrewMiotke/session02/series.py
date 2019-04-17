2c9222663cd36a7682c95d24ca7ddfbcea431bdb
Andrew Miotke <andrewmiotke@gmail.com>
2019-04-16 20:26:59 -0700

    starting on series.py

---
+++ students/AndrewMiotke/session02/series.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+    """Computes through the a series of numbers by summing the previous two numbers, see Fibonacci Number on wikipedia"""
+
+   return n 
+
+fibonacci()
+
+def lucas(n):
+    """The Lucas Numbers are a related series of integers that start with the values 2 and 1 rather than 0 and 1, see Lucas Number on wikipedia"""
+
+    return n
+
+lucas()

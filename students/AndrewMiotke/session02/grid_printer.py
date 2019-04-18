b7d7cd71f2bd96dec37070acdb946e1c3152c03c
Andrew Miotke <andrewmiotke@gmail.com>
2019-04-18 11:17:32 -0700

    grid printer now works and has a docstring

--- students/AndrewMiotke/session02/grid_printer.py
+++ students/AndrewMiotke/session02/grid_printer.py
@@ -1,44 +1,52 @@
 #!/usr/bin/env python3
 
 # Long hand way of creating a grid
-def verbose_grid():
-    print("+",  " - " * 4, "+", " - " * 4, "+")
+# def verbose_grid():
+#     print("+",  " - " * 4, "+", " - " * 4, "+")
 
-    print("|              |              |")
-    print("|              |              |")
-    print("|              |              |")
-    print("|              |              |")
+#     print("|              |              |")
+#     print("|              |              |")
+#     print("|              |              |")
+#     print("|              |              |")
 
-    print("+",  " - " * 4, "+", " - " * 4, "+")
+#     print("+",  " - " * 4, "+", " - " * 4, "+")
 
-    print("|              |              |")
-    print("|              |              |")
-    print("|              |              |")
-    print("|              |              |")
+#     print("|              |              |")
+#     print("|              |              |")
+#     print("|              |              |")
+#     print("|              |              |")
 
-    print("+",  " - " * 4, "+", " - " * 4, "+")
+#     print("+",  " - " * 4, "+", " - " * 4, "+")
 
-verbose_grid()
+# verbose_grid()
 
 # More general grid generator
-def create_beam():
+def create_beam(beams):
     beam = '+ - - - - '
     plus = "+"
     end_line = end=' '
 
-    print(beam * 2 + plus + end_line)
+    print(beam * beams + plus + end_line)
 
-def create_post():
+def create_post(posts):
     post = '|         '
 
-    print(post * 3)
+    print(post * (posts + 1))
 
-def create_grid(cells):
+def create_grid(cells, beams=0, posts=0):
+    """
+    Returns a grid based on the parameters of this function
+    :param arg 1: define the number of cells
+    :param arg 2: define the number of beams required to complete a cell
+    :param arg 3: define the number of posts required to complete a cell
+
+    Example: create_grid(10, 10, 10) would create a 10x10 grid using 10 cells
+    """
     for _ in range(cells):
-        create_beam()
-        create_post()
-        create_post()
-        create_post()
-    create_beam()
+        create_beam(beams)
+        create_post(posts)
+        create_post(posts)
+        create_post(posts)
+    create_beam(beams)
 
-create_grid(5)
+create_grid(5, 5, 5)

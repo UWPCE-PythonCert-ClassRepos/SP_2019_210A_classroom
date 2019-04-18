29e4303f1d873fd0813169495a69f8a44ce29e20
George Kim <gjkim44@gmail.com>
2019-04-17 21:21:20 -0700

    Finished with docstring

--- students/GKim/lesson02/grid_printer.py
+++ students/GKim/lesson02/grid_printer.py
@@ -3,7 +3,8 @@
 def print_row(c): 
     """
     function to print + and - using a 
-    for loop 
+    for loop .  Using input from user the range is -1 because
+    from the first intial print the  section of the column is already created
     """
     print("+", end = " ")  
     for _ in range(c - 1):
@@ -11,15 +12,23 @@ def print_row(c):
     print("- - - - +")
 
 def print_column(c):
+    """
+    prints the "columns" " | " in the grid. just like the row first section is 
+    already created by the input given from user
+    """
     print("|", end = " ")
     for _ in range(c - 1):
         print("        |", end = " ")
     print("        |")
 
 def my_grid(c,r):
+    """
+    function that puts print_row. print_column all together
+
+    """
     print_row(c)
     for _ in range(r):
-        for _ in range(4):
+        for _ in range(4): # range of 4 for how many | that connect each +
             print_column(c)
         print_row(c)
 

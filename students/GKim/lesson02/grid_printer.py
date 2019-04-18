337d19ade7e602262f60c26d114177c30b90bc82
George Kim <gjkim44@gmail.com>
2019-04-17 21:03:04 -0700

    finished grid code, waiting to finish docstring

--- students/GKim/lesson02/grid_printer.py
+++ students/GKim/lesson02/grid_printer.py
@@ -1,14 +1,32 @@
-def print_row(r):
-    print("+", end = " ")
-    for x in range(r -1):
+
+
+def print_row(c): 
+    """
+    function to print + and - using a 
+    for loop 
+    """
+    print("+", end = " ")  
+    for _ in range(c - 1):
         print("- - - - +", end = " ")
     print("- - - - +")
-    return 
-    
-def print_column(c):
-    for x in range(c):
-        print("|      ", end = "")
-        
 
-print(print_row(4))
-# print(print_column(4))
+def print_column(c):
+    print("|", end = " ")
+    for _ in range(c - 1):
+        print("        |", end = " ")
+    print("        |")
+
+def my_grid(c,r):
+    print_row(c)
+    for _ in range(r):
+        for _ in range(4):
+            print_column(c)
+        print_row(c)
+
+
+# c = int(input("Column: "))
+# r = int(input("Row: "))
+
+# my_grid(4,4)
+
+print_column(4)

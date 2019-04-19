ba0d6ba9e5de5ebcf33aa05b2a6259bdd9f11c67
Andrew Miotke <andrewmiotke@gmail.com>
2019-04-19 16:51:55 -0700

    grid_printer finished

--- students/AndrewMiotke/session02/grid_printer.py
+++ students/AndrewMiotke/session02/grid_printer.py
@@ -33,20 +33,19 @@ def create_post(posts):
 
     print(post * (posts + 1))
 
-def create_grid(cells, beams=0, posts=0):
+def create_grid(beams, posts):
     """
     Returns a grid based on the parameters of this function
-    :param arg 1: define the number of cells
-    :param arg 2: define the number of beams required to complete a cell
-    :param arg 3: define the number of posts required to complete a cell
+    :param arg 2: define the number of beams(x axis) required to complete a cell
+    :param arg 3: define the number of posts(y axis) required to complete a cell
 
-    Example: create_grid(10, 10, 10) would create a 10x10 grid using 10 cells
+    Example: create_grid(10, 10) would create a 10x10 grid
     """
-    for _ in range(cells):
+    for _ in range(posts):
         create_beam(beams)
         create_post(posts)
         create_post(posts)
         create_post(posts)
     create_beam(beams)
 
-create_grid(5, 5, 5)
+create_grid(10, 10)

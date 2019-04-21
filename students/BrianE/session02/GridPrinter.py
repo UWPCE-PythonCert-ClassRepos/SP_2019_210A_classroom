e2a7a09f43918cca173a7ba48f5f59977a21cd84
ervbrian <brian_s_ervin@hotmail.com>
2019-04-21 10:28:42 -0700

    Adding initial print_grid() function with single parameter. Also, added details on main() calls showing which functions were used.

--- students/BrianE/session02/GridPrinter.py
+++ students/BrianE/session02/GridPrinter.py
@@ -5,6 +5,32 @@ Prints ascii grid based on desired box size and grid size.
 """
 
 
+def print_grid(n):
+    """
+    Return 2x2 pattern grid with user-defined box size
+    
+    :param n: integer used to determine box sizing
+    :return: string containing ascii grid
+    """
+
+    plus = '+'
+    space = ' '
+    dash = '-'
+    pipe = '|'
+    row_divider = (plus + ((space + dash) * n) + space) * 2 + plus
+    column_middle = ((pipe + (space * (n * 2) + space)) * 2) + pipe
+    output = ''
+
+    # Outer loop adds lines to distinguish between rows
+    # Inner loop adds lines to distinguish between columns
+    for _ in range(2):
+        output += row_divider + '\n'
+        for _ in range(n):
+            output += column_middle + '\n'
+    output += row_divider
+    return output
+    
+
 def grid(box_size=2, grid_size=2):
     """
     Return grid pattern as a string
@@ -33,11 +59,11 @@ def grid(box_size=2, grid_size=2):
 
 
 def main():
-    print('box_size=5 (single argument, default 2x2 grid):', grid(5), sep='\n')
+    print('print_grid() n=5 (single argument, default 2x2 grid):', print_grid(5), sep='\n')
     print()
-    print('box_size=1, grid_size=2:', grid(box_size=1, grid_size=2), sep='\n')
+    print('grid() box_size=1, grid_size=2:', grid(box_size=1, grid_size=2), sep='\n')
     print()
-    print('box_size=2, grid_size=4:', grid(box_size=2, grid_size=4), sep='\n')
+    print('grid() box_size=2, grid_size=4:', grid(box_size=2, grid_size=4), sep='\n')
 
 
 if __name__ == '__main__':

bcd25aebf8acebe3499efb653c1505a098cf0d76
George Kim <gjkim44@gmail.com>
2019-04-21 14:29:16 -0700

    final grid with Ron's approval

--- students/GKim/lesson02/grid_printer.py
+++ students/GKim/lesson02/grid_printer.py
@@ -9,9 +9,13 @@ def print_row(c, a =3):
     from the first intial print the  section of the column is already created
     this all prints accross
     """
+    if c % 2 == 0:
+        x = int(c)/2
+    elif c % 2 == 1:
+        x = int(c-1)/2
     for _ in range(a-1):
         print("+", end = "")
-        print(" - " * ((c-1)//2), end = "")
+        print(" - " * int(x), end = "")
     print("+")
 
 def print_column(c, a =3):
@@ -19,30 +23,40 @@ def print_column(c, a =3):
     prints the "columns" " | " in the grid. just like the row first section is 
     already created by the input given from user.  The colums print across 
     """
+    if c % 2 == 0:
+        x = int(c)/2
+    elif c % 2 == 1:
+        x = int(c-1)/2
     for _ in range(a-1):
         print("|", end = "")
-        print("   " * ((c-1)//2), end = "")
+        print("   " * int(x), end = "")
     print("|")
     
 
-def my_grid(c,a =3):
+def my_grid(c,a=3):
     """
     function that puts print_row. print_column all together for the grid.  All determined
     by the user input
 
     """
+    c = c * 2
+
+    if c % 2 == 0:
+        x = int(c)/2
+    elif c % 2 == 1:
+        x = int(c-1)/2
     print_row(c,a)
-    for _ in range(3-1): # the number of rows in the grid.  
-        for _ in range((c-1)//2): # range of 4 for how many | that connect each +
+    for _ in range(a-1): # the number of rows in the grid.  
+        for _ in range(int(x)): # range of 4 for how many | that connect each +
             print_column(c,a) # moving down
         print_row(c,a)
 
+
+
+
 # print(print_row(4))
 # print(print_column(4))
+my_grid(3,5)
 
 
-
-
-
-my_grid(3)
 

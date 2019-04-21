c9dcda0de89f0923d9b2e991769fe3540f9ac378
George Kim <gjkim44@gmail.com>
2019-04-20 17:58:31 -0700

    modified grid printer

--- students/GKim/lesson02/grid_printer.py
+++ students/GKim/lesson02/grid_printer.py
@@ -2,44 +2,65 @@
 FYI not my code , got help from you tube... but I understand and like this method.
 """
 
-def print_row(c): 
+def print_row(c, a =3): 
     """
     function to print + and - using a 
     for loop .  Using input from user the range is -1 because
     from the first intial print the  section of the column is already created
     this all prints accross
     """
-    print("+", end = " ")  
-    for _ in range(c - 1):
-        print("- - - - +", end = " ")
-    print("- - - - +")
+    if c % 2 == 0:
+        x = int(c)/2
+        for _ in range(a-1):
+            print("+", end = "")
+            print(" - " * int(x), end = "")
+        print("+")
 
-def print_column(c):
+    elif c % 2 == 1:
+        x = int(c-1)/2
+        for _ in range(a-1):
+            print("+", end = "")
+            print(" - " * int(x), end = "")
+        print("+")
+
+def print_column(c, a =3):
     """
     prints the "columns" " | " in the grid. just like the row first section is 
     already created by the input given from user.  The colums print across 
     """
-    print("|", end = " ")
-    for _ in range(c - 1):
-        print("        |", end = " ")
-    print("        |")
+    if c % 2 == 0:
+        x = int(c)/2
+        for _ in range(a-1):
+            print("|", end = "")
+            print("   " * int(x), end = "")
+        print("|")
 
-def my_grid(c,r):
+    elif c % 2 == 1:
+        x = int(c-1)/2
+        for _ in range(a-1):
+            print("|", end = "")
+            print("   " * int(x), end = "")
+        print("|")
+    
+
+def my_grid(c,a =3):
     """
     function that puts print_row. print_column all together for the grid.  All determined
     by the user input
 
     """
-    print_row(c)
-    for _ in range(r): # the number of rows in the grid.  
-        for _ in range(4): # range of 4 for how many | that connect each +
-            print_column(c) # moving down
-        print_row(c)
+    print_row(c,a)
+    for _ in range(3-1): # the number of rows in the grid.  
+        for _ in range((c-1)//2): # range of 4 for how many | that connect each +
+            print_column(c,a) # moving down
+        print_row(c,a)
+
+# print(print_row(4))
+# print(print_column(4))
 
 
-# input command from the user
-c = int(input("Column: "))
-r = int(input("Row: "))
 
-my_grid(c,r)
+
+
+my_grid(9)
 

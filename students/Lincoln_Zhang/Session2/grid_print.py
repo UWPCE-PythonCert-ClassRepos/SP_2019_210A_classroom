b9900faf63d46aafc7370db585823eca8159032e
kanmenlaohan <kanmenlaohan@gmail.com>
2019-04-21 18:52:08 -0500

    update grid_print_1 per comment

--- students/Lincoln_Zhang/Session2/grid_print.py
+++ students/Lincoln_Zhang/Session2/grid_print.py
@@ -21,7 +21,7 @@ print("""
 
 boxsize = int(input("Please input box size: "))
 gridsize = int(input("Please input grid size: "))
-n = int(input("Please input a positive integer: ")) + 1
+n = int(input("Please input a positive integer: ")) 
 
 def horizontal_line(boxsize):
 	return "- " * boxsize + "+ "
@@ -29,26 +29,24 @@ def horizontal_line(boxsize):
 def vertical_line(boxsize):
 	return "  " * boxsize + "| " 
 
-#a scale function for a 4X4 grid
-def grid_print_1(n):
-	if not (n-1)%4:
-		r = "- "*int((n-1)/2) + "+ " + "- "*int((n-1)/2)
-		c = "  "*int((n-1)/2) + "| " + "  "*int((n-1)/2)
 
-		for _ in range(n+2):
-			if _%int((n-1)/2+1) == 0:
-				print ("+ " + r + "+")
-			else:
-				print ("| " + c + "|")
-	else:
-		n = input("Please input an integer n modulo 4 is 0: ")
+def grid_print_1(n):
+	r = "- "*n + "+ " + "- "*n
+	c = "  "*n + "| " + "  "*n
+
+	for _ in range(n*2+2):
+		if not _%(n+1):
+			print ("+ " + r + "+")
+		else:
+			print ("| " + c + "|")
+	print ("+ " + r + "+")
 
 
 def grid_print_2(boxsize, gridsize):
 	r = horizontal_line(boxsize)
 	c = vertical_line(boxsize)
 	for _ in range((boxsize+1)*gridsize+1):
-		if _%(boxsize+1) == 0:
+		if not _%(boxsize+1):
 			print ("+ " + r*gridsize)
 		else:
 			print ("| " + c*gridsize)
@@ -58,8 +56,7 @@ grid_print_1(n)
 
 print("grid_print_2 output: ")
 grid_print_2(boxsize, gridsize)
-
-#print("grid_print_2 is the solution to this assignment")	
+	
 
 # if __name__ == "__main__":
 # 	print("module is imported")

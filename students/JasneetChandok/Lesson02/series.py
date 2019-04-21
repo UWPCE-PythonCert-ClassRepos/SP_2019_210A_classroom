f76a04a061f30b10facf8ae420bbf88e76c1aed9
Chandok <Jasneet.Chandok1@T-Mobile.com>
2019-04-21 09:48:14 -0700

    Updated the code based on feedback

--- students/JasneetChandok/Lesson02/series.py
+++ students/JasneetChandok/Lesson02/series.py
@@ -31,26 +31,28 @@ def sum_series(n, n0=0, n1=1):
 
     :param n0=0: value of zeroth element in the series
     :param n1=1: value of first element in the series
-    
+
     This function should generalize the fibonacci() and the lucas(),
     so that this function works for any first two numbers for a sum series.
     Once generalized that way, sum_series(n, 0, 1) should be equivalent to fibonacci(n).
     And sum_series(n, 2, 1) should be equivalent to lucas(n).
+
+    sum_series(n, 3, 2) should generate another series with no specific name
+
+    The defaults are set to 0, 1, so if you don't pass in any values, you'll
+    get the fibonacci sercies
     """
-    if n0 == False:
-        if n > 1:
-            return fibonacci(n - 1) + fibonacci(n - 2)
-        if n == 1:
-            return n1
-        elif n <= 0:
-            return n0
+    if n0 == 0 and n1 == 1:
+        return fibonacci(n)
+    elif n0 == 2 and n1 == 1:
+        return lucas(n)
     else:
-        if n > 1:
-            return lucas(n - 1) + lucas(n - 2)
-        elif n == 1:
-            return n1
-        elif n <= 0:
+        a, b = n0, n1
+        if n == 0:
             return n0
+        for _ in range(n - 1):
+            a, b = b, a + b
+        return b
     pass
 
 if __name__ == "__main__":
@@ -64,15 +66,24 @@ if __name__ == "__main__":
     assert fibonacci(6) == 8
     assert fibonacci(7) == 13
 
-
     assert lucas(0) == 2
     assert lucas(1) == 1
+
     assert lucas(4) == 7
 
-
+    # test that sum_series matches fibonacci
     assert sum_series(5) == fibonacci(5)
+    assert sum_series(7, 0, 1) == fibonacci(7)
 
     # test if sum_series matched lucas
     assert sum_series(5, 2, 1) == lucas(5)
 
-print("Tests Passed")
+    # test if sum_series works for arbitrary initial values
+    assert sum_series(0, 3, 2) == 3
+    assert sum_series(1, 3, 2) == 2
+    assert sum_series(2, 3, 2) == 5
+    assert sum_series(3, 3, 2) == 7
+    assert sum_series(4, 3, 2) == 12
+    assert sum_series(5, 3, 2) == 19
+
+    print("tests passed")

f990ad18c399c3bd5ab5f6ea9aebdbf028ea194e
pberhe <pberhe90@gmail.com>
2019-04-21 17:02:58 -0700

    finished grid

--- students/PeterB/Session 2/Grid.py
+++ students/PeterB/Session 2/Grid.py
@@ -6,10 +6,10 @@ space = ' '
 # print_grid2(3,4)Width = 4
 
 def print_row(length):
-    print(plus + space + minus * length + plus+space + minus * length + plus)
+    print(plus + space + minus * length + plus+space + minus * length + plus+space + minus * length+ plus)
 
 def print_column(length):
-    print(bar + space+space*length *2 + bar + space+space*length*2 + bar)
+    print(bar + space+space*length *2 + bar + space+space*length*2 + bar+ space+space*length*2 + bar)
 
 
 def grid_print (length):
@@ -19,5 +19,14 @@ def grid_print (length):
             print_column(length)
     print_row(length)
 
-grid_print(3)
+# grid_print(3)
+
+def grid_print2(cells, length):
+    for _ in range(cells):
+        print_row(length)
+        for _ in range(length):
+            print_column(length)
+    print_row(length)
+
+grid_print2(3,4)
 

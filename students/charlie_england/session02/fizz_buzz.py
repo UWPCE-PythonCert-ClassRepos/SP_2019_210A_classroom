4378c3fd4ed01a6addc30a49df4a67c9be74f739
Charlie England <charlie265@live.com>
2019-04-22 10:22:27 -0700

    added a docstring

--- students/charlie_england/session02/fizz_buzz.py
+++ students/charlie_england/session02/fizz_buzz.py
@@ -4,6 +4,10 @@
 # For numbers which are multiples of both three and five print “FizzBuzz” instead.
 
 for num in range(1,101):
+    """
+    goes through a range from 1-100 (inclusive)
+    prints the num except if the num is divisible by 5 (Buzz), 3 (Fizz) or both (FizzBuzz)
+    """
     if num % 3 == 0 and num % 5 == 0:
         print("FizzBuzz")
     elif num % 3 == 0:

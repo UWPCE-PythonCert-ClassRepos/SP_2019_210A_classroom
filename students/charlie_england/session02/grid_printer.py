cdb24669a9db5e83d0ed9337f3b7ff25f3648189
Charlie England <charlie265@live.com>
2019-04-22 10:23:25 -0700

    refined docstring

--- students/charlie_england/session02/grid_printer.py
+++ students/charlie_england/session02/grid_printer.py
@@ -2,6 +2,7 @@
 def pri_grid(size = 2, cols = 2, rows = 2): #instead of just 2 parameters, allows you to change the # of rows and cols independently
     '''
     prints a grid that has a size, number of columns and number of rows
+    default is size 2, 2 columns and 2 rows
     '''
     for _ in range(rows): # for ever row: print the +...- line and a number of the |...| lines
         print((" +" + " -"*size)*cols+" +") #prints the +, - line. Changes number of '-' with the size and the number of + with the number of columns

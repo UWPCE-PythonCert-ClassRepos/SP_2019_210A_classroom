3e1228457c6839f97d083aacc61397a84a6f8702
Andrew Miotke <andrewmiotke@gmail.com>
2019-04-22 14:47:30 -0700

    fixed grid_printer.py

--- students/AndrewMiotke/session02/grid_printer.py
+++ students/AndrewMiotke/session02/grid_printer.py
@@ -1,51 +1,64 @@
 #!/usr/bin/env python3
 
-# Long hand way of creating a grid
-# def verbose_grid():
-#     print("+",  " - " * 4, "+", " - " * 4, "+")
-
-#     print("|              |              |")
-#     print("|              |              |")
-#     print("|              |              |")
-#     print("|              |              |")
-
-#     print("+",  " - " * 4, "+", " - " * 4, "+")
-
-#     print("|              |              |")
-#     print("|              |              |")
-#     print("|              |              |")
-#     print("|              |              |")
-
-#     print("+",  " - " * 4, "+", " - " * 4, "+")
-
-# verbose_grid()
-
-# More general grid generator
-def create_beam(beams):
-    beam = '+ - - - - '
-    plus = "+"
-    end_line = end=' '
-
-    print(beam * beams + plus + end_line)
-
-def create_post(posts):
-    post = '|         '
-
-    print(post * (posts + 1))
-
-def create_grid(beams, posts):
+def verbose_grid():
     """
-    Returns a grid based on the parameters of this function
-    :param arg 2: define the number of beams(x axis) required to complete a cell
-    :param arg 3: define the number of posts(y axis) required to complete a cell
-
-    Example: create_grid(10, 10) would create a 10x10 grid
+    Returns a 2x2 grid
+    This is hard coded and not flexible at all
     """
-    for _ in range(posts):
-        create_beam(beams)
-        create_post(posts)
-        create_post(posts)
-        create_post(posts)
-    create_beam(beams)
+    print("+",  " - " * 4, "+", " - " * 4, "+")
 
-create_grid(10, 10)
+    print("|              |              |")
+    print("|              |              |")
+    print("|              |              |")
+    print("|              |              |")
+
+    print("+",  " - " * 4, "+", " - " * 4, "+")
+
+    print("|              |              |")
+    print("|              |              |")
+    print("|              |              |")
+    print("|              |              |")
+
+    print("+",  " - " * 4, "+", " - " * 4, "+")
+
+verbose_grid()
+
+# Variables to make up grids below
+plus = '+'
+minus = '- '
+post = '|'
+space = ' '
+
+def create_beam(size):
+    print(plus + space + minus * size + plus + space + minus * size + plus)
+
+def create_post(size):
+    print(post + space + space * size * 2 + post + space + space * size * 2 + post + space + space * size * 2)
+    
+def create_grid(size):
+    """
+    Returns a 2x2 grid with flexible cell height and width
+    :param arg 0: define the arguement in the function call to determine the height('|') and width('-') of each cell
+    """
+    for _ in range(2):
+        create_beam(size)
+        for _ in range(size):
+            create_post(size)
+    create_beam(size)
+
+create_grid(5)
+
+def print_grid(cells, size):
+    """
+    Returns a Xx2 grid with flexible cell height and width
+    :param arg 0: defines the number of cells along the y axis
+    :param arg 1: defines the height('|') and width('-') of each cell
+   """
+    for _ in range(cells):
+        create_beam(size)
+        for _ in range(size):
+            create_post(size)
+    create_beam(size)
+
+print_grid(5, 5)
+

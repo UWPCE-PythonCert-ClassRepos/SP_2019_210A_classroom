008b1afb91a1f2602fb7a5c038b67eee5bf8d4dd
George Kim <gjkim44@gmail.com>
2019-04-23 20:08:23 -0700

    added updated py files

--- students/GKim/lesson03/list_lab.py
+++ students/GKim/lesson03/list_lab.py
@@ -45,11 +45,11 @@ def print_list(lst):
     return
 
 def question():
-    response = input("\nPlease enter another fruit: ")
+    response = input("\nPlease enter another fruit: \n")
     return response
 
 def letter_p(lst):
-    print("\nHere are fruits that start with the letter 'P'")
+    print("\nHere are fruits that start with the letter 'P'\n")
     for x in lst:
         if "P" in x:
             print("'P' fruits: ", x)
@@ -68,20 +68,19 @@ def main():
 
     response = question()
     e_list.append(response)
-    new_inv = e_list + inv
+    new_inv = e_list + inv # concatenating two list together
     print_list(new_inv)
 
     response = question()
     new_inv.insert(0,response)
     print_list(new_inv)
 
-    letter_p(new_inv)
+    letter_p(new_inv) # show fruits with the letter "P"
    
     print_list(new_inv)
-    new_inv.pop(-1)
+    new_inv.pop(-1) # removing the last item on the list.
     print("\nNew list with last item removed\n")
     print_list(new_inv)
-
     while True:
         delete_f = input("What fruit would you like to delete? \n")
         for x in new_inv:
@@ -89,8 +88,20 @@ def main():
                 new_inv.remove(delete_f)
                 print(new_inv)
                 return False
-            else:
-                pass
-        print_list(new_inv)
-
+        
+        print("\nPlease check spelling and make sure its in the list\n")
+    
+    
+    print_list(new_inv)
+    new_inv2 = []
+    while True:
+        while True:
+            for x in new_inv:
+                like_fruit = input("Do you like {}?".format(x))
+                if like_fruit.lower() == "y" or like_fruit.lower() == "yes":
+                    new_inv2.append(x.lower())
+                elif like_fruit.lower() == "n" or like_fruit.lower() == "no":
+                    new_inv.remove(x)
+                else:
+                    pass
 if __name__ == "__main__": main()

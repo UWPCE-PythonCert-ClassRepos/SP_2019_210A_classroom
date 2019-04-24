7789823743968d76c790f11079972f12a7cd42fe
JoeNunnelley <joe.nunnelley@socrata.com>
2019-04-24 00:12:55 -0700

    finishing string_formatting and cleaning up

--- students/JoeNunnelley/session03/mailroom.py
+++ students/JoeNunnelley/session03/mailroom.py
@@ -5,16 +5,23 @@ Author : Joe Nunnelley
 """
 import sys
 
+donors = [("George Jetson", [100, 50, 200]),
+          ("Bugs Bunny", [400, 55, 5000]),
+          ("Daffy Duck", [0, 3, 5, 6, 76, 8]),
+          ("Elmer Fudd", [66, 666, 6666, 6666, 66666,]),
+          ("Porky Pig", [0.50, 56.45, 67.89])]
+
 def send_thankyou():
-    print('Sending a Thank You')
-    pass
+    donor = select_donor()
+    print(f'\nSending a Thank You to {donor[0]}...\n')
+
     print()
     print()
 
 
 def print_report():
-    print('Printing Report')
-    pass
+    print('\nPrinting Report...\n')
+
     print()
     print()
 
@@ -22,6 +29,14 @@ def quit():
     print('Mailroom Closing...')
     print()
     sys.exit(0)
+
+def select_donor():
+    for idx, donor in enumerate(donors):
+        print("{}: {}".format(idx, donor[0]))
+
+    index = int(input("Which donor? "))
+
+    return donors[index]
 
 
 def main_menu():
@@ -51,11 +66,6 @@ def run():
     the entry point for the program
     """
     print("Starting Mailroom...")
-    donors = [("George Jetson", [100, 50, 200]),
-              ("Bugs Bunny", [400, 55, 5000]),
-              ("Daffy Duck", [0, 3, 5, 6, 76, 8]),
-              ("Elmer Fudd", [66, 666, 6666, 6666, 66666,]),
-              ("Porky Pig", [0.50, 56.45, 67.89])]
 
     while True:
         main_menu()

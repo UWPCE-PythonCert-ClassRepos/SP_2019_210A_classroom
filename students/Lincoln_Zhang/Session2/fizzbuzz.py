c372c24e88dc1d52f381017968a8cc39b10eebf9
kanmenlaohan <kanmenlaohan@gmail.com>
2019-04-23 22:41:08 -0500

    class change

--- students/Lincoln_Zhang/Session2/fizzbuzz.py
+++ students/Lincoln_Zhang/Session2/fizzbuzz.py
@@ -4,47 +4,48 @@
 # For the multiples of five print “Buzz” instead of the number.
 # For numbers which are multiples of both three and five print “FizzBuzz” instead.
 
+
 def return_Fizz(n):
-	if n%5:
-		if not n%3:
-			return "Fizz"
-		else:
-			return n
-	else:		
-		return n
+    if n % 5:
+        if not n % 3:
+            return "Fizz"
+        else:
+            return n
+    else:       
+        return n
 
 def return_Buzz(n):
-	if n%3:
-		if not n%5:
-			return "Buzz"
-		else:
-			return n
-	else:
-		return n
+    if n % 3:
+        if not n % 5:
+            return "Buzz"
+        else:
+            return n
+    else:
+        return n
 
 def return_FizzBuzz(n):
-	if not n%15:
-		return "FizzBuzz"
-	else:
-		return n
+    if not n % 15:
+        return "FizzBuzz"
+    else:
+        return n
 
 
 def Fizz_Buzz_filter(n):
-	l = [return_Fizz(n),return_Buzz(n),return_FizzBuzz(n)]
-	if "Fizz" in l:
-		return "Fizz"
-	elif "Buzz" in l:
-		return "Buzz"
-	elif "FizzBuzz" in l:
-		return "FizzBuzz"
-	else:
-		return n
+    l = [return_Fizz(n), return_Buzz(n), return_FizzBuzz(n)]
+    if "Fizz" in l:
+        return "Fizz"
+    elif "Buzz" in l:
+        return "Buzz"
+    elif "FizzBuzz" in l:
+        return "FizzBuzz"
+    else:
+        return n
 
 
 def printer(m):
-	for n in range (1,m+1):
-		a = Fizz_Buzz_filter(n)
-		print (a)
+    for n in range (1,m+1):
+        a = Fizz_Buzz_filter(n)
+        print (a)
 
 assert return_Fizz(45) == 45
 assert return_Fizz(9) == "Fizz"

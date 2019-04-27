b90a3de5c8734f7cee6645693f6a7451125b2857
BBrumleve <brian.brum#gmail.com>
2019-04-27 16:11:26 -0700

    adding FINAL strformat_lab.py

--- students/BrianB/lesson03/strformat_lab.py
+++ students/BrianB/lesson03/strformat_lab.py
@@ -1,88 +1,63 @@
 #!/usr/bin/env python3
 
-# --------------------------------------------------------------
-# Task One
-#    Write a format string that will take the
-#       following four element tuple:
+# -----------------
+# Task 1
 #
-#        ( 2, 123.4567, 10000, 12345.67)
+# -----------------
+
+t1 = (2, 123.4567, 10000, 12345.67)
+print("file_00%d:  %.2f, %.2e, %.3e" % t1)
+
+
+# -----------------
+# Task 2
 #
-#        and produce:
-#
-#        'file_002 :   123.46, 1.00e+04, 1.23e+04'
-#
-# Let’s look at each of the four tuple elements in turn:
-# 1) The first element is used to generate a filename that
-#       can help with file sorting. The idea behind the
-#       “file_002” is that if you have a bunch of files
-#       that you want to name with numbers that can be sorted,
-#       you need to “pad” the numbers with zeros to get
-#       the right sort order.
-# 2) The second element is a floating point number.  You
-#       should display it with 2 decimal places shown.
-# 3) The third value is an integer, but could be any
-#       number.  You should display it in scientific notation,
-#       with 2 decimal places shown.
-# 4) The fourth value is a float with a lot of digits
-#       – display it in scientific notation with
-#       3 significant figures.
-# --------------------------------------------------------------
+# -----------------
 
-# print("Task One:")
-# n1 = (2, 123.4567, 10000, 12345.67)
-# print("file_00%d:  %.2f, %.2e, %.3e" % n1)
+print("file_00{:d}:  {:.2f}, {:.2e}, {:.3e}"\
+      .format(2, 123.4567, 10000, 12345.67))
+
+# -----------------
+# Task 3
+# -----------------
 
 
-# --------------------------------------------------------------
-# Task Two
-#   Using your results from Task One, repeat the
-#   exercise, but this time using an alternate type
-#   of format string (hint: think about alternative
-#   ways to use .format() (keywords anyone?), and also
-#   consider f-strings if you’ve not used them already).
-# ---------------------------------------------------------------
-
-# print("\nTask Two:")
-# print("file_00{:d}:  {:.2f}, {:.2e}, {:.3e}"\
-#      .format(2, 123.4567, 10000, 12345.67))
+def formatter(*seq):
+    """ Returns a string calling out the number of inputs along
+        with printing a string formatting of the tuple values"""
+    length = len(seq)
+    task_three = "the {} numbers are: " + ", ".join(["{}"]*length)
+    return task_three.format(length, *seq)
 
 
-# --------------------------------------------------------------
-# Task Three
-#   Using your results from Task One, repeat the
-#   exercise, but this time using an alternate type
-#   of format string (hint: think about alternative
-#   ways to use .format() (keywords anyone?), and also
-#   consider f-strings if you’ve not used them already).
-# ---------------------------------------------------------------
-
-print("Task Three:")
-
-'''
-In [20]: formatter((2,3,5))
-Out[20]: 'the 3 numbers are: 2, 3, 5'
-
-In [21]: formatter((2,3,5,7,9))
-Out[21]: 'the 5 numbers are: 2, 3, 5, 7, 9'
-'''
-# Task Three_2:
-# def formatter(x, y, z):
-#     tup =  "the 3 numbers are: {:d}, {:d}, {:d}".format(x, y, z)
-#     return tup
-#
-#
-# print((formatter(3, 2, 5)))
+print(formatter(3, 2, 5, 8, 9))
 
 
-# Task Three_3
-def formatter(tup):
-    tup = str(tup)
-    counter = 0
-    for i in tup:
-        counter += 1
-    tup = "the " + str(counter) + " numbers are: {:d}, {:d}, {:d}".format(*str)
-    return tup, counter
-print(formatter(2, 3, 5))
+# -----------------
+# Task 4
+# -----------------
+
+task_four = (4, 30, 2017, 2, 27)
+print("{3:0>2d} {4:d} {2:d} {0:0>2d} {1:d}".format(*task_four))
 
 
+# -----------------
+# Task 5
+# -----------------
+
+name = ("orange", "lemon")
+weight = (1.3, 1.1)
+print(f"The weight of an {name[0].upper()} is {weight[0]*1.2} and the\
+ weight of a {name[1].upper()} is {weight[1]*1.2}.")
+
+
+# -----------------
+# Task 6
+# -----------------
+
+name_age_cost = [("Ghost", 15, 35000), ("Lady", 6, 410),
+                 ("Nymeria", 15, 15100), ("Shaggydog", 12, 5469)]
+
+for item in name_age_cost:
+    print("{:17}{:0>2d}{:9,}".format(*item))
 

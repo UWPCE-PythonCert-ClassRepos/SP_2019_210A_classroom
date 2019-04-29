85338f0b6bb8fbafad584e070efad8abfb8b987e
Arch Wu <archwu0817@gmail.com>
2019-04-28 18:35:40 -0700

    finished implementation of report()

--- students/ArchWu/Session3/mailroom.py
+++ students/ArchWu/Session3/mailroom.py
@@ -1,21 +1,37 @@
 #!/usr/local/bin/python3
 import sys
+import operator
 
+#donor (Donor name, donations[])
+#donors [donor1, donor2]
 donors = []
 
-def add_donor():
-    return
-def get_donor():
-    return
+def add_donor(name):
+    donations = []
+    return (name, donations)
+
 def report():
+    print("Donor Name                | Total Given | Num Gifts | Average Gift")
+    print("------------------------------------------------------------------")
+    donors.sort(reverse = True, key = lambda item: (sum(item[1])))
+    for donor in donors:
+        name, total, num_gifts, avg = gen_stats(donor)
+        part_1 = "{}{}".format(name, ' ' * (27 - len(name)))
+        part_2 = "${}{}".format(' ' * (11 - len(str(total))), total)
+        part_3 = "{}{}{}{}{}".format(' ' *(12 - len(str(num_gifts))), num_gifts,'  $', ' ' * (12 - len(str(avg))), avg)
+        result = part_1 + part_2 + part_3
+        print(result)
     return
 
 def gen_stats(donor):
     name = donor[0]
     donations = donor[1]
-    total = sum(donations)
-    avg = total / len(donations)
-    return (name, total, avg)
+    total = round(sum(donations),2)
+    num_gifts = len(donations)
+    avg = round((total / num_gifts), 2)
+    return (name, total, num_gifts, avg)
+
+def thank_you():
 
 def main_menu():
     while True:
@@ -36,12 +52,7 @@ Pick one:
         else:
             print("please answer 1, 2 or 3")
 
-    quit = False
-    # while(quit):
-    #     #do something
-    #     add_donor()
-    # return
-
 if __name__ == '__main__':
+    donors = [('William Gates, III', [100000, 800227.556]), ('Mark Zuckerberg', [10000, 8697.67]), ('Jeff Bezos', [100, 1255]), ('Paul Allen', [100, 200, 3000, 5000, 10000])]
     print("Welcome to the mailroom!")
     main_menu()

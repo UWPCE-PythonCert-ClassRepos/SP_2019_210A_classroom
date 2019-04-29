cb99a7597faa0673af99cff1aea120686c29a381
Charlie England <charlie265@live.com>
2019-04-28 23:40:13 -0700

    added files slicing_lab and list_lab

---
+++ students/charlie_england/session03/slicing_lab.py
@@ -0,0 +1,9 @@
+# Tasks
+# Write some functions that take a sequence as an argument, and return a copy of that sequence:
+
+# with the first and last items exchanged.
+# with every other item removed.
+# with the first 4 and the last 4 items removed, and then every other item in the remaining sequence.
+# with the elements reversed (just with slicing).
+# with the last third, then first third, then the middle third in the new order.
+

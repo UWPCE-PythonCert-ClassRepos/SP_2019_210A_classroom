3639cbb7634e28a6992f8face4e24d5ab808a31f
Andrew Miotke <andrewmiotke@gmail.com>
2019-04-30 12:47:55 -0700

    Working on the other labs

---
+++ students/AndrewMiotke/session03/slicing_lab.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# https://uwpce-pythoncert.github.io/PythonCertDevel/exercises/slicing.html
+
+# Global variables to slice
+string_one = "here is the first string"
+string_two = "just another string to be sliced"
+string_three = "Slicing is great and should be used when possible."
+
+def slicing_one(seg):
+    return seg
+
+
+if __name__ == "__main__":
+    print(slicing_one(string_one[1:-1]))

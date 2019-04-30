f2cdf6dc3c4639e7c6a09206e9face9c6af3929d
Andrew Miotke <andrewmiotke@gmail.com>
2019-04-29 20:37:41 -0700

    mailroom can now search for donors, add donors, and list donors

--- students/AndrewMiotke/session03/mailroom.py
+++ students/AndrewMiotke/session03/mailroom.py
@@ -1,50 +1,63 @@
 #!/usr/bin/env python3
 import sys
 
-donors = [("Rufio"), [100, 200, 200],
-          ("Maggie"), [100, 2, 3000],
-          ("Gus"), [100, 32, 33222],
-          ("Kramer"), [10, 87, 886],
-          ("Polly"), [432, 32, 7896]
+donors_list = [("Rufio", [897, 200, 200]),
+          ("Maggie", [543, 2, 3000]),
+          ("Gus", [23, 32, 33222]),
+          ("Kramer", [10, 87, 886]),
+          ("Polly", [432, 32, 7896])
          ]
 
 
 def thank_you():
-    get_donors_name = input("Please enter your full name: ")
-    
-    for i in donors:
-        if i == get_donors_name:
-            print(f"match with {i}")
-        else:
-            print("no dice")
+    find_donor()
 
 
 def gen_stats(donor):
     donations = donor[1]
     total = sum(donations)
     num = len(donations)
-    stats = (donor[0], total, num, total / num) 
-    
+    stats = (donor[0], total, num, total / num)
+
     return stats
+
 
 def report():
     print("Create a report")
+    gen_stats(donor)
+
+def find_donor():
+    print("Type 'list' to get a list of donors")
+    donor_name = input("Type in the name of a donor: ")
+
+    if donor_name == "list":
+        print("\nHere is your list of donors:\n")
+        for donor in donors_list:
+            print(f"Donors are: {donor}")
+    else:
+        for donor in donors_list:
+            if donor_name == donor[0]:
+                print(f"Found donor: {donor_name}")
+        else:
+            donors_list.append(donor_name)
+            print(f"Adding {donor_name} to the list of donors.")
+
 
 
 def quit():
-    sys.exit(0)    
+    sys.exit(0)
 
 
 def main_menu():
     while True:
-        answer = input("""What you would you like to do?
+        answer = input(""" -> What you would you like to do?
     Pick One:
     1: Send a thank you
     2: Create a report
     3: Quit
     >>> """)
         print(f"You selected {answer}")
-        print(answer)
+
         answer = answer.strip()
         if answer == "1":
             thank_you()
@@ -61,6 +74,6 @@ if __name__ == "__main__":
     print("Welcome to the Mailroom")
 
     donor = ("fred flinstone"), [100, 50, 600]
-    # assert gen_stats(donor) == ("fred flinstone"), [750, 3, 250.0] 
+    # assert gen_stats(donor) == ("fred flinstone"), [750, 3, 250.0]
 
     main_menu()

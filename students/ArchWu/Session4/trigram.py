a6ebe895aa55fc5de4f13835262d55bf73043f6a
archwu <archwu0817@gmail.com>
2019-04-30 20:17:07 -0700

    done building build_trigrams()

--- students/ArchWu/Session4/trigram.py
+++ students/ArchWu/Session4/trigram.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+import random
+import string
 
 words = "I wish I may I wish I might".split()
-
 
 def build_trigrams(words):
     """
@@ -12,18 +13,39 @@ def build_trigrams(words):
        values: list of followers
     """
     trigrams = {}
-    words = words.split()
-    for i in range(0, len(words - 2)):
+    #words = words.split()
+    for i in range(0, len(words) - 2):
         word1 = words[i]
         word2 = words[i + 1]
         word3 = words[i + 2]
-        trigrams[(word1, word2)] = word3
+        if (word1, word2) not in trigrams:
+            trigrams[(word1, word2)] = [word3]
+        else:
+            trigrams[(word1, word2)].append(word3)
+
 
     # build up the dict here!
 
     return trigrams
 
+def make_words(in_data):
+
+def read_in_data(filename):
+
+def build_text():
+
 
 if __name__ == "__main__":
-    trigrams = build_trigrams(words)
-    print(trigrams)
+    # get the filename from the command line
+    try:
+        filename = sys.argv[1]
+    except IndexError:
+        print("You must pass in a filename")
+        sys.exit(1)
+
+    in_data = read_in_data(filename)
+    words = make_words(in_data)
+    word_pairs = build_trigram(words)
+    new_text = build_text(word_pairs)
+
+    print(new_text)

cc0cc44adc71d82c4b33bc78d56065e9068d98ad
colephalen <colephalen@gmail.com>
2019-05-04 14:14:24 -0700

    adding slicing

---
+++ students/ColeP/Session03/slicing.py
@@ -0,0 +1,65 @@
+# Write some functions that take a sequence as an argument, and return a copy of that sequence:
+
+# with the first and last items exchanged.
+# with every other item removed.
+# with the first 4 and the last 4 items removed, and then every other item in the remaining sequence.
+# with the elements reversed (just with slicing).
+# with the last third, then first third, then the middle third in the new order.
+
+a_string = "this is a string"
+a_tuple = (2, 54, 13, 12, 5, 32)
+
+
+def switch_first_last(seq):
+    first = seq[0:1]
+    last = seq[-1:]
+    seq = seq[1:-1]
+    seq = last + seq + first
+    return seq
+
+
+# print(switch_first_last(a_string))
+# print(switch_first_last(a_tuple))
+
+
+def every_other(seq):
+    seq = seq[0::2]
+    return seq
+
+
+# print(every_other(a_string))
+# print(every_other(a_tuple))
+
+
+def first4_last4(seq):
+    # first4 = seq[0:5]
+    # last4 = seq[-4:]
+    seq = seq[4:-4:2]
+    return seq
+
+
+# print(first4_last4(a_string))
+# print(first4_last4(a_tuple))
+
+def reverse_reverse(seq):
+    seq = seq[::-1]
+    return seq
+
+
+# print(reverse_reverse(a_string))
+# print(reverse_reverse(a_tuple))
+
+def thirds(seq):
+    first_third = seq[:(len(seq)//3)]
+    middle_third = seq[len(seq)//3:-len(seq)//3]
+    last_third = seq[-(len(seq)//3):]
+    
+    return last_third + first_third + middle_third
+
+
+print(len(a_string))
+print(len(a_tuple))
+
+print(thirds(a_string))
+print(thirds(a_tuple))
+

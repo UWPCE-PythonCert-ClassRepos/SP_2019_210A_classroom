21a039e5d00830b138957e7936936511ce869aa4
BBrumleve <brian.brum#gmail.com>
2019-05-03 22:15:19 -0700

    adding lesson04 dict_lab.py IN WORK approx 95% complete

--- students/BrianB/lesson04/dic_lab.py
+++ students/BrianB/lesson04/dic_lab.py
@@ -1,7 +1,5 @@
 #! bin/usr/env python3
 
-print("Miriam Webster")
-
 '''
 Dictionaries 1
     Create a dictionary containing “name”, “city”, and “cake” for
@@ -20,9 +18,66 @@ Dictionaries 1
             (i.e. True).
 
 Dictionaries 2
-    Using the dictionary from item 1: Make a dictionary using 
-        the same keys but with the number of ‘t’s in each value 
+    Using the dictionary from item 1: Make a dictionary using
+        the same keys but with the number of ‘t’s in each value
         as the value (consider upper and lower case?).
-        
+
 '''
+
+
+def dict_1():
+    d1 = {
+        'name': 'Chris',
+        'city': 'Seattle',
+        'cake': 'Chocolate'
+        }
+
+    # display dictionary
+    print('Original Dictionary: ', d1)
+
+    # delete key, cake
+    del d1['cake']
+
+    # display dictionary without cake
+    print('\nDictionary with key "cake" deleted: ', d1, '\n')
+
+    # add dictionary with a fruit
+    d1.update({'fruit': 'Mango'})
+
+    # display dictionary with new key and value
+    print('Key, value with fruit, mango added:')
+    for (key, value) in d1.items():
+        print(key, ': ', value)
+
+    # display the dictionary keys
+    print('\n', d1.keys())
+
+    # display the dictionary values
+    print('\n', d1.values())
+
+    # test if 'cake' is no longer a key
+    for key, value in d1.items():
+        if key == 'fruit':
+            print('\nIs cake a key? ', False)
+
+    # test if 'mango' is a value
+    for key, value in d1.items():
+        if value == 'Mango':
+            print('\nIs mango a value?', True, '\n')
+
+    # print number of t's in values
+    # expect to see {name: 0, city: 2, fruit: 0}
+    for key, value in d1.items():
+        for item in value:
+            if 't' in item.lower():
+                print(key, ':', len(item))
+            else:
+                print(key, ':', 0)
+            break
+    return d1
+
+
+# display dictionary keys:
+dict_1()
+
 

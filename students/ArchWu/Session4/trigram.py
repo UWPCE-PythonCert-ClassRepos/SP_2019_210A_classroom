8d31ff51083eebdd1ef80b507231e0301820fc8b
Arch Wu <archwu0817@gmail.com>
2019-05-04 22:59:07 -0700

    a somehow working version of trigram

--- students/ArchWu/Session4/trigram.py
+++ students/ArchWu/Session4/trigram.py
@@ -3,8 +3,9 @@ import random
 import string
 
 words = "I wish I may I wish I might".split()
+trigrams = {}
 
-def build_trigrams(words):
+def build_trigram(words):
     """
     build up the trigrams dict from the list of words
 
@@ -12,9 +13,9 @@ def build_trigrams(words):
        keys: word pairs
        values: list of followers
     """
-    trigrams = {}
+
     #words = words.split()
-    for i in range(0, len(words) - 2):
+    for i in range(0, len(words) - 3):
         word1 = words[i]
         word2 = words[i + 1]
         word3 = words[i + 2]
@@ -27,9 +28,9 @@ def build_trigrams(words):
 
     return trigrams
 
-def make_words(in_data):
-
-    return
+# def make_words(in_data):
+#
+#     return
 
 def read_in_data(filename):
     """
@@ -38,24 +39,46 @@ def read_in_data(filename):
     """
     in_data = []
     with open(filename) as f:
-        in_data.append(f.split())
+        for line in f:
+            for word in line.split():
+                in_data.append(word)
     #f.close()
     return in_data
 
-def build_text():
+def build_text(word_pairs):
+    initial_random = random.randint(0, len(word_pairs) - 1)
+    length_limit = 1000
+    word_count = 0
+    text = []
+    # for i, word_pair, val in enumerate(word_pairs):
+    #     if i == initial_random:
+    #         initial_pair = word_pair
+    #         initial_word = val
+    #         text.append(word_pair)
+    #         text.append(val)
+    #         break
+    initial_pair = random.choice(list(word_pairs))
+    initial_val = random.choice(word_pairs[initial_pair])
+    text.extend(initial_pair)
+    text.append(initial_val)
 
+    while(word_count < length_limit):
+        new_pair = (text[-2], text[-1])
+        new_val = random.choice(word_pairs[new_pair])
+        text.append(new_val)
+        word_count += 1
+    return ' '.join(text)
 
 if __name__ == "__main__":
     # get the filename from the command line
     try:
-        filename = sys.argv[1]
+        filename = 'sherlock.txt'
     except IndexError:
         print("You must pass in a filename")
         sys.exit(1)
 
     in_data = read_in_data(filename)
-    words = make_words(in_data)
-    word_pairs = build_trigram(words)
+    word_pairs = build_trigram(in_data)
     new_text = build_text(word_pairs)
 
     print(new_text)

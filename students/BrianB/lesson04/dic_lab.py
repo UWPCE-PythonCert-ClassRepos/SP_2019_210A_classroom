fa8fbdfa33dedbc633010a3077b7d01a1aa54984
BBrumleve <brian.brum#gmail.com>
2019-05-05 20:09:56 -0700

    uplaoding FINAL dic_lab.py

--- students/BrianB/lesson04/dic_lab.py
+++ students/BrianB/lesson04/dic_lab.py
@@ -1,36 +1,15 @@
 #! bin/usr/env python3
 
-'''
-Dictionaries 1
-    Create a dictionary containing “name”, “city”, and “cake” for
-        “Chris” from “Seattle” who likes “Chocolate” (so the keys
-        should be: “name”, etc, and values: “Chris”, etc.)
-        
-    Display the dictionary.
-    Delete the entry for “cake”.
-    Display the dictionary.
-    Add an entry for “fruit” with “Mango” and display the dictionary.
-        Display the dictionary keys.
-        Display the dictionary values.
-        Display whether or not “cake” is a key in the dictionary
-            (i.e. False) (now).
-        Display whether or not “Mango” is a value in the dictionary
-            (i.e. True).
-
-Dictionaries 2
-    Using the dictionary from item 1: Make a dictionary using
-        the same keys but with the number of ‘t’s in each value
-        as the value (consider upper and lower case?).
-
-'''
+d = {
+    'name': 'Chris',
+    'city': 'Seattle',
+    'cake': 'Chocolate'
+    }
 
 
 def dict_1():
-    d1 = {
-        'name': 'Chris',
-        'city': 'Seattle',
-        'cake': 'Chocolate'
-        }
+    """Working with a dictionary"""
+    d1 = d
 
     # display dictionary
     print('Original Dictionary: ', d1)
@@ -46,7 +25,7 @@ def dict_1():
 
     # display dictionary with new key and value
     print('Key, value with fruit, mango added:')
-    for (key, value) in d1.items():
+    for key, value in d1.items():
         print(key, ': ', value)
 
     # display the dictionary keys
@@ -64,20 +43,22 @@ def dict_1():
     for key, value in d1.items():
         if value == 'Mango':
             print('\nIs mango a value?', True, '\n')
-
-    # print number of t's in values
-    # expect to see {name: 0, city: 2, fruit: 0}
-    for key, value in d1.items():
-        for item in value:
-            if 't' in item.lower():
-                print(key, ':', len(item))
-            else:
-                print(key, ':', 0)
-            break
     return d1
+
+
+def dict_2():
+    """Working with a dictionary"""
+    d2 = d
+    print("The count of 't's in each value are: ")
+    while True:
+        for key, value in d2.items():
+            print(key, ':', value.lower().count('t'))
+        break
+    return d2
 
 
 # display dictionary keys:
 dict_1()
 
-
+# dictionary 2 function call
+dict_2()

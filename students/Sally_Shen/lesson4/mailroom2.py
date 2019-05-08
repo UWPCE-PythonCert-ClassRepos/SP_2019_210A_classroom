a6813ddb67eb7b3c767cd8e0cb89bc84de3b8225
sallysy8 <sallysy8@hotmail.com>
2019-05-07 19:32:43 -0700

    fixed some formatting issues

--- students/Sally_Shen/lesson4/mailroom2.py
+++ students/Sally_Shen/lesson4/mailroom2.py
@@ -3,11 +3,14 @@
 '''choms'''
 
 import sys
+import os
 
 print("in mailroom")
 '''Writing first name is easier for testing.'''
-donors ={"Fred Gates": [100.90, 200, 50], "Jeff Bazos": [100, 50, 600], "Mark Zuckerberg": [200, 50, 500],
+donors ={"Fred Gates": [100.90, 200, 50], "Jeff Bezos": [100, 50, 600], "Mark Zuckerberg": [200, 50, 500],
          "Paul Allen": [300, 40, 200]}
+
+#donors = {{"first_name": "Fred", "last_name": "Gates"}: [[100.90, 200, 50], 265.98]}
 
 
 def thank_you():
@@ -26,7 +29,7 @@ def thank_you():
     email = '''
     Dear {0},
     Thank you for donating {1}!\n'''.format(response, newDonation)
-    print(email)
+    return email
 
 
 def gen_stats(donor):
@@ -46,8 +49,8 @@ def report():
             amountToNames[total] = [name]
 
     sortedAmount = sorted(amountToNames, reverse=True)
-    output = "{:<15}${:<15}{:<12}${:<12}\n".format("Donor Name", "|Total Given", "|Num Gifts", "|Average Gift")
-    output += "-------------------------------------------------------------\n"
+    output = "{:<20}{:<20}{:<15}{:<20}\n".format("Donor Name", "|Total Given", "|Num Gifts", "|Average Gift")
+    output += "-------------------------------------------------------------------\n"
 
 
     for amount in sortedAmount:
@@ -58,8 +61,29 @@ def report():
     print(output)
 
 def lineItem(name, total, numGifts, avg):
-    output = "{:<15}${:<15}{:<12}${:<12.2f}\n".format(name, total, numGifts, avg)
+    output = "{:<20}${:<20.2f}{:<15}${:<20.2f}\n".format(name, total, numGifts, avg)
     return output
+
+def gen_thank_you_letters(a_dict):
+    dir = "thankyouLetters"
+    try:
+        os.mkdir(dir)
+    except FileExistsError:
+        pass
+    for name, amount in donors.items():
+        firstLastName = name.split()
+        fileName = firstLastName[0] + "_" + firstLastName[1] + ".txt"
+        filePath = dir + "\\" + fileName
+        #print(filePath)
+        with open(filePath, "w") as f:
+            f.write('''
+            Dear {0},
+                    Thank you for your very kind donation of ${1:.2f}.
+                    
+                    It will be put to very good use.
+                    
+                                        Sincerely,
+                                            - The Team'''.format(name, sum(amount)))
 
 def quit():
     print("quitting")
@@ -72,9 +96,10 @@ def main_menu():
     while True:
         answer = input ("""What would you like to do?
     Pick one:
-    1. Send a Thank You
+    1. Send a Thank You to a single donor
     2. Create a Report
-    3. Quit
+    3. Send Thankyou letters to all donors
+    4. Quit
     >>>""")
         print("You picked:", answer)
         answer = answer.strip()
@@ -83,9 +108,11 @@ def main_menu():
         elif answer == "2":
             report()
         elif answer == "3":
+            gen_thank_you_letters(donors)
+        elif answer == "4":
             quit()
         else:
-            print("Please answer 1, 2, 3")
+            print("Please answer 1, 2, 3, 4")
 
 
 if __name__ == '__main__':

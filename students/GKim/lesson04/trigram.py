12324bcb736c074bf3cedbc97526bb688c4bd260
George Kim <gjkim44@gmail.com>
2019-05-07 19:09:08 -0700

    finished trigrams with Chris's solution to finish assingment

--- students/GKim/lesson04/trigram.py
+++ students/GKim/lesson04/trigram.py
@@ -2,6 +2,7 @@
 # WORDS = "with his head sunk upon his chest and his hands clasped behind him.".split()
 # WORDS = "I wish I may I wish I might".split()
 
+import random
 
 punctuations = "!" + "-" + "," + "." + "(" + ")" + '"' + '?' + ';' + ":" + '\n'
 replacements = " " * 11
@@ -20,7 +21,7 @@ def punctuation_removal(text_line, punc, repl):
 
 def read_file(filename):
     """
-    returns list of words in the file
+    returns list of words in the file clear of unwanted punctuations
     """
     words = []
     with open(filename,"r") as text:
@@ -56,18 +57,40 @@ def build_trigrams(words):
 
     return trigrams
 
-# def make_text(trigrams):
-#     pass
+def make_text(trigrams):
+    """
+    Chris's solution
+    """
+
+    new_text = []
+    for i in range(10):  # do ten sentences
+        # pick a word pair to start the sentence
+        # need to make dict.keys() a list to randomly select from it
+        sentence = list(random.choice(list(trigrams.keys())))
+        # now add a random number of additional words to the sentence
+        for j in range(random.randint(2, 10)):
+            pair = tuple(sentence[-2:])  # the next word pair is the last two words
+            sentence.append(random.choice(trigrams[pair]))
+
+        # capitalize the first word:
+        sentence[0] = sentence[0].capitalize()
+
+        # Add the period
+        sentence[-1] += "."
+        new_text.extend(sentence)
+
+    new_text = " ".join(new_text)
+    return new_text
 
 
 def main():
     # read_file("test.txt")
-    words = read_file("test.txt")
+    # read_file("test.txt")
+    in_file = read_file("sherlock_small.txt")
     # print(words)
-    trigrams = build_trigrams(words)
+    trigrams = build_trigrams(in_file)
+    final_text = make_text(trigrams)
    
-    print(trigrams)
-    for k, v in trigrams.items():
-        print(k, v)
+    print(final_text)
 
 if __name__ == "__main__": main()

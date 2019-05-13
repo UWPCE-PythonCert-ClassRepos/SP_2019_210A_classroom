c65b2cbd0ddfbd589fae9bb0af0ba5021221e861
George Kim <gjkim44@gmail.com>
2019-05-12 20:59:26 -0700

    uploading examples from unittest and pytest

---
+++ students/GKim/lesson06/test_my_mod.py
@@ -0,0 +1,14 @@
+import unittest
+from my_mod import my_func
+
+class MyTests(unittest.TestCase):
+
+    def test_my_func(self):
+        test_vals = (2, 3)
+        expected = test_vals[0] * test_vals[1]
+        actual = my_func(*test_vals)
+        self.assertEqual(expected, actual)
+
+
+if __name__ == "__main__":
+    unittest.main()

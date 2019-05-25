200c484db08e6ece87a977a0b0aa11690a776c00
George Kim <gjkim44@gmail.com>
2019-05-24 17:25:34 -0700

    adding homework and exercise files

---
+++ students/GKim/lesson07/html_render.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+A class-based system for rendering html.
+"""
+
+
+# This is the framework for the base class
+class Element:
+
+    tag = "html"
+
+    def __init__(self, content=None):
+       self.contents = [content]
+
+    def append(self, new_content):
+        self.contents.append(new_content)
+        
+
+    def render(self, out_file):
+        out_file.write("<{}>\n".format(self.tag))
+        for content in self.contents:
+            out_file.write(content)
+            out_file.write("\n")
+        out_file.write("</{}>\n".format(self.tag))
+

8f1818eb0acdde3bd0b2c523f6f743510cc451f8
Andrew Miotke <andrewmiotke@gmail.com>
2019-05-26 16:46:28 -0700

    removed sample_html and added some tests for step 1

--- students/AndrewMiotke/session07/HTML_Renderer/html_render.py
+++ students/AndrewMiotke/session07/HTML_Renderer/html_render.py
@@ -6,13 +6,23 @@ A class-based system for rendering html.
 
 
 # This is the framework for the base class
-class Element():
+class Element: # Did we need the () in Python3+ ?
+
+    tag = "html"
 
     def __init__(self, content=None):
-        pass
+        self.contents = [content]
+
 
     def append(self, new_content):
-        pass
+        self.contents.append(new_content)
+
 
     def render(self, out_file):
-        out_file.write("just something as a place holder...")
+        out_file.write(f"<{self.tag}>\n")
+
+        for content in self.contents:
+            out_file.write(content)
+            out_file.write("\n")
+
+        out_file.write(f"</{self.tag}>")

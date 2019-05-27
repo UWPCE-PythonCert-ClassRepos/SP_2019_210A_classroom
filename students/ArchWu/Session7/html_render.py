a743394fe642ce31ba4613878d80f7a2dd5e3a93
Arch Wu <archwu0817@gmail.com>
2019-05-27 15:26:54 -0700

    step 1 done

--- students/ArchWu/Session7/html_render.py
+++ students/ArchWu/Session7/html_render.py
@@ -6,13 +6,32 @@ A class-based system for rendering html.
 
 
 # This is the framework for the base class
-class Element(object):
+class Element():
+
+    tag = 'html'
 
     def __init__(self, content=None):
-        pass
+        self.contents = [content]
 
     def append(self, new_content):
-        pass
+        self.contents.append(new_content)
 
     def render(self, out_file):
-        out_file.write("just something as a place holder...")
+        out_file.write("<html>")
+        for content in self.contents:
+            out_file.write(content)
+        out_file.write("</html>")
+
+
+class Body(Element):
+
+    tag = 'body'
+
+    def __init__(self, content=None):
+        return super().__init__(content=content)
+
+    def append(self, new_content):
+        return super().append(new_content)
+    
+    def render(self, out_file):
+        return super().render(out_file)

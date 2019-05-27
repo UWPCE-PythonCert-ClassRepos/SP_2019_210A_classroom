b43bd632f41e0e78f193b6c50a9ad6bec58ff768
Andrew Miotke <andrewmiotke@gmail.com>
2019-05-27 15:53:42 -0700

    adding tests and making progress of the renderer

--- students/AndrewMiotke/session07/HTML_Renderer/html_render.py
+++ students/AndrewMiotke/session07/HTML_Renderer/html_render.py
@@ -10,9 +10,13 @@ class Element: # Did we need the () in Python3+ ?
 
     tag = 'html'
 
-    def __init__(self, content=None):
-        self.contents = [content]
-        print("contents is:", self.contents)
+
+    def __init__(self, content=None, **kwargs):
+        self.__dict__.update(kwargs) # might not need this right now
+        if content:
+            self.contents = [content]
+        else:
+            self.contents = []
 
 
     def append(self, new_content):
@@ -22,16 +26,31 @@ class Element: # Did we need the () in Python3+ ?
         else:
             self.contents.append(new_content)
 
-
+    # Chris's initial render method
     def render(self, out_file):
-        if self.contents:
-            out_file.write(f'<{self.tag}>\n')
-
-            for content in self.contents:
+        # loop through the list of contents:
+        for content in self.contents:
+            out_file.write("<{}>\n".format(self.tag))
+            try:
+                content.render(out_file)
+            except AttributeError:
                 out_file.write(content)
-                out_file.write('\n')
+            out_file.write("\n")
+            out_file.write("</{}>\n".format(self.tag))
 
-            out_file.write(f'</{self.tag}>\n')
+
+    # my render method
+    # def render(self, out_file):
+    #     out_file.write(f'<{self.tag}>\n')
+
+    #     for content in self.contents:
+    #         try:
+    #             content.render(out_file)
+    #         except AttributeError:
+    #             out_file.write(content)
+    #         out_file.write('\n')
+
+    #     out_file.write(f'</{self.tag}>\n')
 
 
     def create_page(self):
@@ -42,14 +61,46 @@ class Element: # Did we need the () in Python3+ ?
             page.render(out_file)
 
 
+e = Element()
+e.create_page()
+
+
+class OneLineTag(Element):
+    def render(self, out_file):
+        open_tag = [f"<{self.tag}"]
+        open_tag.append(">\n")
+        out_file.write("".join(open_tag))
+        # out_file.write(f"<{self.tag}>")
+        out_file.write(self.contents[0])
+        out_file.write(f"</{self.tag}>\n")
+
+
+    def append(self, content):
+        raise NotImplementedError
+
+
+
+class Title(OneLineTag):
+    tag = 'title'
+
+
 
 class Html(Element):
     tag = 'html'
+
+
+
+class Head(Element):
+    tag = "head"
+
 
 
 class Body(Element):
     tag = 'body'
 
 
+
 class P(Element):
     tag = 'p'
+
+

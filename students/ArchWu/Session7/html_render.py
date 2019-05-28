6b893e804230be9e41d3496b96e73cf50ca4002b
Arch Wu <archwu0817@gmail.com>
2019-05-27 19:18:25 -0700

    task 3 done

--- students/ArchWu/Session7/html_render.py
+++ students/ArchWu/Session7/html_render.py
@@ -70,3 +70,43 @@ class P(Element):
     
     def render(self, out_file):
         return super().render(out_file)
+
+
+class Head(Element):
+    tag = "head"
+
+    def __init__(self, content=None):
+        return super().__init__(content=content)
+    
+    def append(self, new_content):
+        return super().append(new_content)
+    
+    def render(self, out_file):
+        return super().render(out_file)
+    
+class OneLineTag(Element):
+    tag = 'oneliner'
+    def __init__(self, content=None):
+        return super().__init__(content=content)
+    
+    def append(self, new_content):
+        return super().append(new_content)
+    
+    def render(self, out_file):
+        head = "<{}>".format(self.tag)
+        for content in self.contents:
+            head += content
+        endings = "</{}>".format(self.tag)
+        out_file.write(head + endings)
+
+class Title(OneLineTag):
+    tag = 'title'
+    def __init__(self, content=None):
+        return super().__init__(content=content)
+    
+    def append(self, new_content):
+        return super().append(new_content)
+    
+    def render(self, out_file):
+        return super().render(out_file)
+    

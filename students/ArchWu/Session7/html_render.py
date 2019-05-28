165d9fb6954b4241274e6682c3cf2ece5b98c746
Arch Wu <archwu0817@gmail.com>
2019-05-27 18:25:14 -0700

    task 2 done

--- students/ArchWu/Session7/html_render.py
+++ students/ArchWu/Session7/html_render.py
@@ -7,21 +7,36 @@ A class-based system for rendering html.
 
 # This is the framework for the base class
 class Element():
-
     tag = 'html'
 
     def __init__(self, content=None):
-        self.contents = [content]
+        self.contents = []
+        if content: self.contents.append(content)
 
     def append(self, new_content):
         self.contents.append(new_content)
 
     def render(self, out_file):
-        out_file.write("<html>")
         for content in self.contents:
-            out_file.write(content)
-        out_file.write("</html>")
+            out_file.write("<{}>".format(self.tag))
+            try:
+                content.render(out_file)
+            except AttributeError:
+                out_file.write(content)
+            out_file.write("</{}>".format(self.tag))
 
+class Html(Element):
+    tag = 'html'
+
+    def __init__(self, content=None):
+        return super().__init__(content=content)
+
+    def append(self, new_content):
+        return super().append(new_content)
+    
+    def render(self, out_file):
+        return super().render(out_file)
+    
 
 class Body(Element):
 
@@ -31,7 +46,27 @@ class Body(Element):
         return super().__init__(content=content)
 
     def append(self, new_content):
-        return super().append(new_content)
+        self.contents.append("<body>")
+        self.contents.append(new_content)
+        self.contents.append("</body>")
+        
+    def render(self, out_file):
+        out_file.write("<body>")
+        for content in self.contents:
+            out_file.write(content)
+        out_file.write("</body>")
+
+class P(Element):
+
+    tag = 'p'
+
+    def __init__(self, content=None):
+        return super().__init__(content=content)
+    
+    def append(self, new_content):
+        self.contents.append("<p>")
+        self.contents.append(new_content)
+        self.contents.append("</p>")
     
     def render(self, out_file):
         return super().render(out_file)

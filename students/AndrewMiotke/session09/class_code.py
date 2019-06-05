3e90e1157bd23a666c9cb2725d4c4b06e3b1f083
Andrew Miotke <andrewmiotke@gmail.com>
2019-06-04 19:30:58 -0700

    multiple inhertiance

--- students/AndrewMiotke/session09/class_code.py
+++ students/AndrewMiotke/session09/class_code.py
@@ -1,3 +1,5 @@
+# Example 1
+
 class A:
     def __init__(self):
         print("In class A")
@@ -55,3 +57,51 @@ class D(B, C):
         print("D", n)
         super().num(n)
 
+
+
+# ---------------------------------------------------------------------
+# Example 2
+# Word tokenizer
+
+class Tokenizer:
+
+    def __init__(self, text):
+        self.tokens = text.split()
+
+
+    def describe(self):
+        return self.tokens
+
+
+class WordCounter(Tokenizer):
+    def __init__(self, text):
+        super().__init__(text)
+        self.word_count = len(self.tokens)
+
+
+    def describe(self):
+        return self.tokens, self.word_count
+
+
+class Vocabulary(Tokenizer):
+    def __init__(self, text):
+        super().__init__(text)
+        self.vocab = set(self.tokens)
+
+
+    def describe(self):
+        return self.tokens, self.vocab
+
+
+class TextDescriber(WordCounter, Vocabulary):
+    def __init__(self, text):
+        super().__init__(text)
+        """
+        Inheriting Classes:
+         - WordCount
+         - Tokenizer
+         - Vocabulary
+         """
+
+    def describe(self):
+        return self.tokens, self.word_count, self.vocab

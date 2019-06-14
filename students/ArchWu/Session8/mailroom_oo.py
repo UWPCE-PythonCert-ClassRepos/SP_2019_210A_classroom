de0d1e90fee8a3f784e5edbd8bb52bafc967c785
archwu <archwu0817@gmail.com>
2019-06-13 17:55:59 -0700

    done testings for add, find, report

--- students/ArchWu/Session8/mailroom_oo.py
+++ students/ArchWu/Session8/mailroom_oo.py
@@ -2,6 +2,14 @@
 import sys, os
 from textwrap import dedent
 
+def init_donor_db():
+    db = DonorCollection()
+    db.add_donor(Donor("William Gates III", [653772.32, 12.17]))
+    db.add_donor(Donor("Jeff Bezos", [877.33]))
+    db.add_donor(Donor("Paul Allen", [663.23, 43.87, 1.32]))
+    db.add_donor(Donor("Mark Zuckerberg", [1663.23, 4300.87, 10432.0]))
+    return db
+
 class Donor:
     def __init__(self, name, donations = None):
         self.norm_name = self.normalize_name(name)
@@ -17,7 +25,7 @@ class Donor:
     
     @staticmethod
     def normalize_name(name):
-        return name.lower().strip().replace("", " ")
+        return name.lower().strip().replace(" ", "")
     
     @property
     def total(self):
@@ -37,6 +45,8 @@ class Donor:
         return self.name, self.total, len(self.donations), self.average
 
 class DonorCollection():
+
+    # Singleton setup
     __instance = None
 
     @staticmethod
@@ -53,23 +63,23 @@ class DonorCollection():
             self.donors = {}
             
     
-    def add_donor(self, name):
-        self.donors[Donor.normalize_name(name)] = Donor(name)
+    def add_donor(self, donor):
+        self.donors[Donor.normalize_name(donor.name)] = donor
     
     def get_donor(self, name):
         return self.donors[Donor.normalize_name(name)]
     
-    @staticmethod
     def list_donor(self):
         listing = ["Donor list:"]
         for donor in self.donors:
-            listing.append(donor.name)
+            print(self.donors)
+            listing.append(self.get_donor(donor.strip()).name)
         return "\n".join(listing)
 
     def report(self):
         """ Make a report on donations received by a style as specified """
-        sorted_donors = sorted(self.donors.items(), reverse = True, key = lambda item: item.total())
-        report_rows = ["{0:25s}   {1:10.2f}   {2:9d}   {3:11.2f}".format(*donor.gen_stats()) for donor in sorted_donors]
+        sorted_donors = sorted(self.donors.items(), reverse = True, key = lambda item: item[1].total)
+        report_rows = ["{0:25s}   {1:10.2f}   {2:9d}   {3:11.2f}".format(*donor[1].gen_stats()) for donor in sorted_donors]
         report = []
         report.append("{:25s} | {:11s} | {:9s} | {:12s}".format("Donor Name",
                                                                 "Total Given",
@@ -127,5 +137,5 @@ def goodbye():
 
 
 if __name__ == "__main__":
-    db = DonorCollection()
+    init_donor_db()
     main()
